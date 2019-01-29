f468888e682acf77297bc606cc5b7d8c1f77e62b
ArchAtom19 <31146804+ArchAtom19@users.noreply.github.com>
2019-01-29 12:39:34 +0000

    Add files via upload

--- aquasite/aboutus.php
+++ aquasite/aboutus.php
@@ -13,6 +13,8 @@
     <body>
         <div id="mainArea">
             <span class="aquaPageTitle">ABOUT US</span>
+            
+            <p>Project AQUA (<span style="color: rgb(13, 167, 255)">A</span>ir <span style="color: rgb(13, 167, 255)">Q</span>uality in <span style="color: rgb(13, 167, 255)">U</span>rban <span style="color: rgb(13, 167, 255)">A</span>cademia) is a venture launched by students of York College designed to measure the levels of air pollution near various educational institutions within the City of York.</p>
         </div>
     </body>
     
