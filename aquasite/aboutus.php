fc7916475653ae4bd5cd33b584ab46ad66157190
ArchAtom19 <31146804+ArchAtom19@users.noreply.github.com>
2019-02-05 13:33:35 +0000

    Adding updated web files.

--- aquasite/aboutus.php
+++ aquasite/aboutus.php
@@ -12,9 +12,44 @@
     
     <body>
         <div id="mainArea">
-            <span class="aquaPageTitle">ABOUT US</span>
             
-            <p>Project AQUA (<span style="color: rgb(13, 167, 255)">A</span>ir <span style="color: rgb(13, 167, 255)">Q</span>uality in <span style="color: rgb(13, 167, 255)">U</span>rban <span style="color: rgb(13, 167, 255)">A</span>cademia) is a venture launched by students of York College designed to measure the levels of air pollution near various educational institutions within the City of York.</p>
+            <section>
+                <h1 class="aquaPageTitle">ABOUT US</h1>
+
+                <p>Project AQUA (<span style="color: rgb(13, 167, 255)">A</span>ir <span style="color: rgb(13, 167, 255)">Q</span>uality in <span style="color: rgb(13, 167, 255)">U</span>rban <span style="color: rgb(13, 167, 255)">A</span>cademia) is a venture launched by students of York College designed to measure the levels of air pollution near various educational institutions within the City of York area.</p>
+            </section>
+            
+            <section>
+                <h1 class="aquaPageTitle">MEET THE TEAM</h1>
+                
+                <div id="aquaTeamGallery">
+                    <div class="aquaTeamPortrait">
+                        <img src="img/aquaGalleryBlank.png"/>
+                        <hr />
+                        Ben Gladders<br />
+                        <span class="aquaTeamRole">Project Manager</span>
+                    </div>
+                    <div class="aquaTeamPortrait">
+                        <img src="img/aquaGalleryBlank.png"/>
+                        <hr />
+                        Mark Goodwin<br />
+                        <span class="aquaTeamRole">Lead Software Developer and Web Designer</span>
+                    </div>
+                    <div class="aquaTeamPortrait">
+                        <img src="img/aquaGalleryBlank.png"/>
+                        <hr />
+                        Abdoulie Jammeh<br />
+                        <span class="aquaTeamRole">Lead Tester</span>
+                    </div>
+                    <div class="aquaTeamPortrait">
+                        <img src="img/aquaGalleryBlank.png"/>
+                        <hr />
+                        Cameron Clarke<br />
+                        <span class="aquaTeamRole">Hardware Manager</span>
+                    </div>                    
+                </div>
+            </section>
+            
         </div>
     </body>
     
