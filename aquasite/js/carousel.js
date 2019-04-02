7bb9639ed7a64ea093a5fcd908595d6ca06cb19a
SLIPdiggers <44338673+SLIPdiggers@users.noreply.github.com>
2019-04-02 10:25:43 +0100

    Add files via upload

--- aquasite/js/carousel.js
+++ aquasite/js/carousel.js
@@ -3,61 +3,33 @@
  * For the original version of this script, see carousel_old.js
  */
 
-var caroIndex = 1;
-var caro; //For the entire carousel.
-var chooser; //For the dots that let the user choose which slide to move to.
+var slideIndex = 1;
+showSlides(slideIndex);
 
-firstRun();
-
-function firstRun(){
-   /* This is the function that is triggered as soon as the script is loaded.
-    * This function initialises the slideshow.
-    */
-    caroIndex = 1;
-    
-    console.log("Initialising carousel...")
-    
-    //First slide.
-    document.getElementById("firstSlide").display = "block";
-    document.getElementById("firstSlide").className += "_active";
-    
-    //First "dot."
-    document.getElementById("pick1").className += "_active";
+// Next/previous controls
+function advance(n) {
+  showSlides(slideIndex += n);
 }
 
-function advance(amount){
-    //Advance the slideshow by a specified amount.
-    showSlide(caroIndex += amount);
-    console.log(caroIndex); //Log the carousel index for debugging.
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function setSlide(amount){
-    showSlide(caroIndex = amount);
-    console.log(caroIndex);
-}
-
-function showSlide(position){
-    let i;
-    
-    caro = document.getElementsByClassName("aquaIndexSlide");
-    chooser = document.getElementsByClassName("aquaSlidePick");
-    
-    //If the position is now greater than the number of slides in the carousel, set the index back to the beginning.
-    if (position > caro.length) caroIndex = 1;
-    
-    //Likewise, if the position set is below 1, set the position to the number of slides in the carousel.
-    if (position < 1) caroIndex = caro.length;
-    
-    for(i = 0; i < caro.length; i++){
-        caro[i].style.display = "none";
-    }
-    
-    for(i = 0; i < chooser.length; i++){
-        chooser[i].className = chooser[i].className.replace("_active", "");
-    }
-    
-    caro[caroIndex - 1].style.display = "block";
-    chooser[caroIndex - 1].className += "_active";
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("aquaIndexSlide");
+  var dots = document.getElementsByClassName("aquaSlidePick");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
 }
 
 function confirmInfo(){
